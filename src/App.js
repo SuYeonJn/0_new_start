6cefb41b617350a8b1a9c4e98abe21c0ecccb613
SuYeonJn <suyeoni0218@gmail.com>
2023-01-19 15:47:29 +0900

    react crud 완성본

--- src/App.js
+++ src/App.js
@@ -63,19 +63,48 @@ function CreateForm(props){
         const gender = e.target.gender_input.value;
         props.onCreate(start, arrival, gender);
       }}>
-        <input type="text" name="start_input" placeholder="출발지"></input>
-        <input type="text" name="arrival_input" placeholder="도착지"></input>
+        <input type="text" name="start_input" placeholder="출발지" required></input>
+        <input type="text" name="arrival_input" placeholder="도착지" required></input>
         <br></br>
         <div>성별</div>
-        <input type='radio' name='gender_input' value='여성' />여성
-        <input type='radio' name='gender_input' value='남성' />남성
-        <input type='radio' name='gender_input' value='혼성' />혼성
+        <input type='radio' name='gender_input' value='여성' required/>여성
+        <input type='radio' name='gender_input' value='남성' required/>남성
+        <input type='radio' name='gender_input' value='혼성' required/>혼성
         <p><input type="submit" value="방 만들기"/></p>
       </form>
     </article>
   )
 }
 
+function UpdateForm(props){
+  const [start, setStart] = useState(props.start);
+  const [arrival, setArrival] = useState(props.arrival);
+  return(
+    <article>
+      <h2>수정하기</h2>
+      <form onSubmit={(e)=>{
+        e.preventDefault();
+        const start= e.target.start_input.value;
+        const arrival= e.target.arrival_input.value;
+        const gender = e.target.gender_input.value;
+        props.onUpdate(start, arrival, gender);
+      }}>
+      <input type="text" name="start_input" value={start} required onChange={(e)=>{
+        setStart(e.target.value);
+      }}></input>
+        <input type="text" name="arrival_input" value={arrival} required onChange={(e)=>{
+        setArrival(e.target.value);
+      }}></input>
+        <br></br>
+        <div>성별</div>
+        <input type='radio' name='gender_input' value='여성' required/>여성
+        <input type='radio' name='gender_input' value='남성' required/>남성
+        <input type='radio' name='gender_input' value='혼성' required/>혼성
+        <p><input type="submit" value="수정하기"/></p>
+      </form>
+    </article>
+  )
+}
 
 function App() {
   const [topics, setTopics] = useState([
@@ -83,18 +112,24 @@ function App() {
     {id:2, start:"사당역", arrival:"서울대 학생회관", gender:"남성"},
     {id:3, start:"서울대 농생대", arrival:"서울대입구역", gender:"혼성"}
   ]);
+
   const [mode, setMode] = useState('welcome');
   const [id, setId] = useState(null);
   const [nextId, setNextId] =useState(4);
+
   let content = null;
+  let contextControl = null;
+
   if(mode === "welcome"){
+
+    contextControl = 
+    <Create title="방 만들기"onChangemode={()=>{
+      setMode("create")
+    }}></Create>
+
     content= 
     <div>
       <Greeting title="0" article = "안녕하세요. Mobility입니다"></Greeting>
-      <br></br>
-      <Create title="방 만들기"onChangemode={()=>{
-        setMode("create")
-      }}></Create>
     </div>
   } else if(mode ==="read"){
     let start, arrival, gender = null;
@@ -105,13 +140,33 @@ function App() {
         gender = topics[i].gender;
       }
     }
-    content= 
+
+    contextControl = 
     <div>
-      <Container start={start} arrival = {arrival} gender={gender}></Container>
-      <br></br>
       <Create title="방 만들기"onChangemode={()=>{
         setMode("create")
       }}></Create>
+      <a href= {"/update/"+id} onClick={(e)=>{
+        e.preventDefault();
+        setMode('update');
+      }}>수정하기</a> <br></br>
+      <input type = "button" onClick={()=>{
+        //e.preventDefault();
+        //버튼은 할 필요 없음
+        const newTopics = []
+        for (let i =0; i< topics.length; i++){
+        if(topics[i].id !== id){
+          newTopics.push(topics[i]);
+        }
+        }
+        setTopics(newTopics);
+        setMode("welcome");
+      }} value="삭제하기"/>
+    </div>
+
+    content= 
+    <div>
+      <Container start={start} arrival = {arrival} gender={gender}></Container>      
     </div>
   } else if(mode ==="create"){
     content = <CreateForm onCreate={(start, arrival, gender)=>{
@@ -125,7 +180,28 @@ function App() {
       //다음 입력을 위해 id + 1 해주기
       setNextId(nextId+1);
     }}></CreateForm>
+  } else if(mode==="update"){
+    let start, arrival, gender = null;
+    for(let i =0; i<topics.length; i++){
+    if(topics[i].id === id){
+      start= topics[i].start;
+      arrival = topics[i].arrival;
+      gender = topics[i].gender;
+    }
   }
+    content = <UpdateForm start={start} arrival = {arrival} gender={gender} onUpdate={(start, arrival, gender)=>{
+      const NewTopics = [...topics]
+      const updatedTopic = {id:id, start:start, arrival:arrival, gender:gender}
+      for (let i =0; i<NewTopics.length; i++){
+        if(NewTopics[i].id === id){
+          NewTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      setTopics(NewTopics);
+      setMode("read")
+    }}></UpdateForm>
+  } 
 
   return (
     <div className="App">
@@ -138,9 +214,11 @@ function App() {
       }}></List>
       {content}
       <br></br>
-      
+      {contextControl}
+    
     </div>
   );
 }
 
 export default App;
+
